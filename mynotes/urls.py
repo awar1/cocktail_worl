603e2beba110a2ec50536b9df27d6740fa746277
awar1 <s205826@sggw.edu.pl>
2023-06-18 15:57:55 +0200

    delete_front

--- mynotes/urls.py
+++ mynotes/urls.py
@@ -22,6 +22,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('', TemplateView.as_view(template_name='index.html')),
+    # path('', TemplateView.as_view(template_name='index.html')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
